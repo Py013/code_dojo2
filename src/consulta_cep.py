efa6ec253aab94105635a6b3e0b010dd667cf615
ferauche <thiago.ferauche@gmail.com>
2019-12-03 23:41:25 -0300

    codigo refatorado para demonstrar pytest

--- src/consulta_cep.py
+++ src/consulta_cep.py
@@ -1,19 +1,26 @@
 import requests
 
-print('### CEP ###')
-cep = input('Digite o CEP..: ')
-
-if len(cep) != 8:
-    print("Entre com 8 digitos de cep sem o traço")
-else:
+def getCep(cep):
     request = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
-    dados = request.json()
-    if 'erro' in dados:
-        print("CEP não encontrado!")
+    return request.json()
+
+def main():
+    print('### CEP ###')
+    cep = input('Digite o CEP..: ')
+
+    if len(cep) != 8:
+        print("Entre com 8 digitos de cep sem o traço")
     else:
-        print(dados)
-        print('Logradouro...: '+dados['logradouro'])
-        print('Numero.......: '+dados['complemento'])
-        print('Bairro.......: '+dados['bairro'])
-        print('Cidade.......: '+dados['localidade'])
-        print('Estado.......: '+dados['uf'])
+        dados = getCep(cep)
+        if 'erro' in dados:
+            print("CEP não encontrado!")
+        else:
+            print(dados)
+            print('Logradouro...: '+dados['logradouro'])
+            print('Numero.......: '+dados['complemento'])
+            print('Bairro.......: '+dados['bairro'])
+            print('Cidade.......: '+dados['localidade'])
+            print('Estado.......: '+dados['uf'])
+
+if __name__ == "__main__":
+    main()
